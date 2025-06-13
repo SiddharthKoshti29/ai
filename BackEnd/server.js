8551093a2718e9b9048fd85d08dcfd990af3287c
SiddharthKoshti29 <siddharthkoshti7470@gmail.com>
2025-06-13 20:27:31 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,5 +5,5 @@ const app = require('./src/app')
 
 app.listen(3000, () =>{
 
-    console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on https://ai-backend-hos1.onrender.com')
 })
